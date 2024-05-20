22b895ad55cae076b9046a27419bf290b2edc94b
zifengchenll <zifengchen_ll@163.com>
2024-05-20 15:32:07 +0800

    fix(.github_split/split_before.sh): :bug: 同步修复脚本问题

--- .github_split/split_before.sh
+++ .github_split/split_before.sh
@@ -13,27 +13,29 @@ workdir=$rootdir/../
 
 # 查找并打印 .git 目录路径
 git_paths=$(find $workdir -name ".git")
-echo "找到的 .git 目录路径：" >git_paths.txt
-for git_path in $git_paths; do
-	echo $git_path >>git_paths.txt
-done
 
 # 打印调试信息：找到的 .git 目录路径
 echo "##################################################"
 echo ""
-echo "找到的 .git 目录："
-cat git_paths.txt
+echo "找到的 .git 目录：(规则对子仓库进行拆分忽略)"
+echo "$git_paths"
 echo ""
-echo "##################################################"
 
 # 构建 -path 选项以排除 .git 目录
 exclude_paths=""
 for git_path in $git_paths; do
-	exclude_paths="$exclude_paths -path $git_path -prune -o"
+	# 使用dirname命令提取路径
+	dir_path=$(dirname "$git_path")
+	# 将提取出的路径追加到exclude_paths变量中
+	exclude_paths="$exclude_paths -path $dir_path -prune -o"
 done
 
 # 明确排除工作目录中的 .git
 exclude_paths="$exclude_paths -path $workdir.git -prune -o"
+
+# 打印 exclude_paths 变量
+echo "拆分排除路径选项：$exclude_paths"
+echo ""
 
 # 查找大于 100M 的文件，排除 .git 目录
 split_list=$(eval "find $workdir $exclude_paths -type f -size +100M -print")
@@ -46,7 +48,6 @@ for split_path in $split_list; do
 	echo $split_path
 done
 echo ""
-echo "##################################################"
 
 # 处理找到的大文件
 for split_path in $split_list; do
